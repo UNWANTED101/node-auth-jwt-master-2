9b61f0515102930ab6d08f46c471314ba69f026a
UNWANTED101 <haritz942013@gmail.com>
2020-06-26 13:35:54 +0700

    bgst

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 var corsOptions = {
   //origin: "https://article-irfan.herokuapp.com"
-  origin:"lean-slope.surge.sh"
+  origin:"spotty-nail.surge.sh"
 };
 
 
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Hello Nikka." });
+  res.json({ message: "Hello Nigka." });
 });
 
 
